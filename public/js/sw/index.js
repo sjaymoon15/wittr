15b0ca022387a9248015b5f744eba9d566691b47
Seungjae Moon <sjaymoon15@gmail.com>
2018-01-27 13:28:52 -0800

    respond with sw cache if match, if not fetch the request

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -20,4 +20,9 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   // Leave this blank for now.
   // We'll get to this in the next task.
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  )
 });
