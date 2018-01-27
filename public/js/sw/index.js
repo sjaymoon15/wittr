699f18a7df93bf83452e0d670de701246ab59454
Seungjae Moon <sjaymoon15@gmail.com>
2018-01-27 13:58:50 -0800

    install a new sw and remove the old sw on activate state

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,28 +1,39 @@
 self.addEventListener('install', function(event) {
-  var urlsToCache = [
-    '/',
-    'js/main.js',
-    'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  ];
-
   event.waitUntil(
-    // TODO: open a cache named 'wittr-static-v1'
-    // Add cache the urls from urlsToCache
-    caches.open('wittr-static-v1').then((cache) => {
-      return cache.addAll(urlsToCache);
+    // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
+    // Ensure at least $primary-color changes
+    // TODO: change cache name to 'wittr-static-v2'
+    caches.open('wittr-static-v2').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const currentCaches = ['wittr-static-v2'];
+  event.waitUntil(
+    // TODO: remove the old cache
+    caches.keys().then(cacheList => {
+      return Promise.all(cacheList.map(function(cacheName) {
+        if (currentCaches.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      }))
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  // Leave this blank for now.
-  // We'll get to this in the next task.
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
-  )
+  );
 });
